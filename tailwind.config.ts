7d3536bbb6b5482d321430e6497a026efe1ed049
Salomegit <sallygithinji72@gmail.com>
2024-09-04 22:18:26 +0300

    new charts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        sallySky:"#C3EBFA",
+        sallySkyLight:"#EDF9FD",
+        sallyPurple:"#CFCEFF",
+        sallyPurpleLight:"#F1F0FF",
+        sallyYellow:"#FAE27C",
+        sallyYellowLight:"#FEFCE8",
+
+      }
     },
   },
   plugins: [],
